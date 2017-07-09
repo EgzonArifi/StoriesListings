ca8f1774b781bb2b2b53bbfd3dfd6a446076b0b6
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-07-09 19:27:26 +0200

    redirent to home on logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFire} from 'angularfire2';
 import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,6 +12,7 @@ export class NavbarComponent implements OnInit {
 
   constructor(
     public af:AngularFire,
+    private router:Router,
     public flashMessage:FlashMessagesService
   ) { }
 
@@ -25,6 +27,7 @@ export class NavbarComponent implements OnInit {
     this.af.auth.logout();
     this.flashMessage.show('You are logged out',
     {cssClass: 'alert-success', timeout: 3000});
+    this.router.navigate(['/']);
   }
 
 }
